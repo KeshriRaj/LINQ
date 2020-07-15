b382d07eb5f2d93e5876fc5aaf2aab03f78f91cc
unknown <kunalkeshri047@gmail.com>
2020-07-16 01:58:32 +0530

    Initial Commit

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -10,16 +10,23 @@ namespace Day3
             Console.WriteLine("***********************************************WITHOUT USING LINQ************************************************");
 
             UserRepository obj = new UserRepository();
-            List<User> user = new List<User>();
+            List<User> user = new List<User>();// Declaring list of users
+
+
+            // printing all the users details by calling function
             Console.WriteLine();
             Console.WriteLine("List of Users without using LINQ");
-            user = obj.Users();
+            user = obj.Users();// Calling the function and  Getting the value of all users
             Console.WriteLine("Id \t Name \t Email \t \t \tLocation \tAddress\t\tActiveStatus");
             foreach (User users in user)
             {
                 Console.WriteLine(users.id + "\t" + users.name + "\t" + users.email + "\t" + users.location + "\t" + users.address + "\t  " + users.isActive);
             }
             Console.WriteLine();
+
+
+
+            // Get user details by id by calling function
             Found:
             Console.WriteLine("Enter the id for User you want to retrieve");
             int idUser = Convert.ToInt32(Console.ReadLine());
@@ -35,10 +42,15 @@ namespace Day3
                 Console.WriteLine("Id \t Name \t Email \t \t \tLocation \tAddress\t\tActiveStatus");
                 Console.WriteLine(getUser.id + "\t" + getUser.name + "\t" + getUser.email + "\t" + getUser.location + "\t" + getUser.address + "\t  " + getUser.isActive);
             }
-           
             Console.WriteLine();
 
 
+
+
+
+
+
+            // Deleting the id entered by  users from List
             Console.WriteLine("Enter the id for User you want to Delete");
             int idUser2 = Convert.ToInt32(Console.ReadLine());
 
@@ -53,6 +65,12 @@ namespace Day3
             }
 
 
+
+
+
+
+
+            // Function to get the list of Active Users
             List<User> activeUser = new List<User>();
 
             activeUser = obj.ActiveUsers();
@@ -60,12 +78,18 @@ namespace Day3
 
 
             Console.WriteLine("List of Active Users without using LINQ");
-
             Console.WriteLine("Id \t Name \t Email \t \t \tLocation \tAddress\t\tActiveStatus");
             foreach (User users in activeUser)
             {
                 Console.WriteLine(users.id + "\t" + users.name + "\t" + users.email + "\t" + users.location + "\t" + users.address + "\t  " + users.isActive);
             }
+
+
+
+
+
+
+            //  Addition of New User in the List by calling function
 
             User obj2 = new User();
             obj2.id = 100;
@@ -75,7 +99,6 @@ namespace Day3
             obj2.address = "Bihar\t";
             obj2.isActive = true;
             Console.WriteLine();
-
             Console.WriteLine("After addition of new user in List");
             obj.AddUser(obj2);
             user = obj.Users();
@@ -85,7 +108,31 @@ namespace Day3
                 Console.WriteLine(users.id + "\t" + users.name + "\t" + users.email + "\t" + users.location + "\t" + users.address + "\t  " + users.isActive);
             }
             Console.WriteLine();
+
+
+
+
+
+
+
+            // USING LINQ
             Console.WriteLine("*****************************************************USING LINQ********************************************************");
+           
+
+
+            // USER DETAILS
+            Console.WriteLine();
+            Console.WriteLine("Users Details:- ");
+            Console.WriteLine("Id \t Name \t Email \t \t \tLocation \tAddress\t\tActiveStatus");
+            foreach (User users in user)
+            {
+                Console.WriteLine(users.id + "\t" + users.name + "\t" + users.email + "\t" + users.location + "\t" + users.address + "\t  " + users.isActive);
+            }
+
+
+
+            // RETRIEVE USER DETAILS USING ID
+
             Console.WriteLine();
             Console.WriteLine("Enter the id for User you want to retrieve");
             int idUser1 = Convert.ToInt32(Console.ReadLine());
@@ -96,11 +143,19 @@ namespace Day3
                 Console.WriteLine(users.id + "\t" + users.name + "\t" + users.email + "\t" + users.location + "\t" + users.address + "\t  " + users.isActive);
             }
 
+
+
+
+
+
+            // DELETE USER WITH THE ID GIVEN BY USER
             Console.WriteLine("Enter the id for User you want to Delete");
 
             int delUser = Convert.ToInt32(Console.ReadLine());
 
             user.RemoveAll(x => x.id == delUser);
+
+            Console.WriteLine("List of Users after Deleting User with ID =" + delUser + " using LINQ");
 
             Console.WriteLine("Id \t Name \t Email \t \t \tLocation \tAddress\t\tActiveStatus");
             foreach (User users in user)
@@ -108,8 +163,12 @@ namespace Day3
                 Console.WriteLine(users.id + "\t" + users.name + "\t" + users.email + "\t" + users.location + "\t" + users.address + "\t  " + users.isActive);
             }
 
-            var ActiveUsersLinq = user.Where(users => users.isActive == true);
 
+
+
+
+            // ACTIVE USER LIST
+            var ActiveUsersLinq = user.Where(users => users.isActive == true);
             Console.WriteLine();
             Console.WriteLine("List of Active Users using LINQ");
 
@@ -121,21 +180,26 @@ namespace Day3
             }
             User newObject = new User();
 
-            Console.WriteLine("Enter Users Details");
+
+            // ENETER THE USER DETAILS YOU WANT TO ADD 
+
+            Console.WriteLine();
+            Console.WriteLine("Enter Users Details you want to add in the list");
 
             Console.WriteLine("Enter User Id");
             int id = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("Enter User Name");
             string name = Console.ReadLine();
 
             Console.WriteLine("Enter User Email");
-            string email = Console.ReadLine();
+            string email = Console.ReadLine()+"\t";
 
             Console.WriteLine("Enter User Location");
-            string location = Console.ReadLine();
+            string location = Console.ReadLine()+"\t";
 
             Console.WriteLine("Enter User Address");
-            string  address= Console.ReadLine();
+            string  address= Console.ReadLine()+"\t";
 
             Console.WriteLine("Enter User Activity Status");
             bool activity = Convert.ToBoolean(Console.ReadLine());
@@ -146,12 +210,11 @@ namespace Day3
             Console.WriteLine();
 
             Console.WriteLine("After addition of new user in List");
-            obj.AddUser(obj2);
-            user = obj.Users();
+
             Console.WriteLine("Id \t Name \t Email \t \t \tLocation \tAddress\t\tActiveStatus");
             foreach (User users in user)
             {
-                Console.WriteLine(users.id + "\t" + users.name + "\t" + users.email + "\t\t" + users.location + "\t" + users.address + "\t  " + users.isActive);
+                Console.WriteLine(users.id + "\t" + users.name + "\t" + users.email + "\t" + users.location + "\t" + users.address + "\t  " + users.isActive);
             }
             Console.WriteLine();
 
